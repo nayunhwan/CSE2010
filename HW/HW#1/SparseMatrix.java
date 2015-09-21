593299a82ac48bdee0e7985874f0e8ac1cef33c8
NaYunHwan <kbk9288@gmail.com>
2015-09-21 20:31:04 +0900

    Commit
    Commit

--- HW/HW#1/SparseMatrix.java
+++ HW/HW#1/SparseMatrix.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class SparseMatrix  {
 	// nested class for each non-zero entry in Sparse Matrix
 
+	// ArrayList<Entry> entrys = new ArrayList<Entry>();
 	ArrayList<Entry> entrys = new ArrayList<Entry>();
-
-	int max_x, max_y;
+	int max_x, max_y, count;
 
 	class Entry {
 		int row;
@@ -27,8 +27,52 @@ public class SparseMatrix  {
 	 * You can define additional private fields and/or methods here, if necessary.
 	 */
 	
-	
-	
+	public void entry_swap(int index_i, int index_j){
+		Entry tmp = entrys.get(index_i);
+		entrys.set(index_i, entrys.get(index_j));
+		entrys.set(index_j, tmp);
+	}
+
+	public void entry_sort(){
+		// row 정렬
+		for(int i = 0; i < entrys.size() - 1; i++){
+			for(int j = 0; j < entrys.size() - 1 - i; j++){
+				if(entrys.get(j).row > entrys.get(j+1).row){
+					// Entry tmp = entrys.get(j);
+					// entrys.set(j, entrys.get(j+1));
+					// entrys.set(j+1, tmp);
+					entry_swap(j,j+1);
+				}
+			}
+		}
+
+
+		int front = 0;
+		int back = 0;
+
+		// col 정렬
+
+		for(int i = 0; i < entrys.size() - 1; i++){
+			if(entrys.get(i).row != entrys.get(i+1).row){
+				back = i+1;
+				entry_sort_part(front, back);
+				front = back;
+			}
+		}
+	}
+
+	public void entry_sort_part(int front, int back){
+		for(int i = front; i < back - 1; i++){
+			for(int j = front; j < back - i - 1; j++){
+				if(entrys.get(j).col > entrys.get(j+1).col){
+					// Entry tmp = entrys.get(j);
+					// entrys.set(j, entrys.get(j+1));
+					// entrys.set(j+1, tmp);
+					entry_swap(j,j+1);
+				}
+			}
+		}
+	}
 	
 	
 	/*
@@ -37,26 +81,47 @@ public class SparseMatrix  {
 	public SparseMatrix(String filename) {
 		try{
 			BufferedReader br = new BufferedReader(new FileReader(filename));
-			int x = 0;
-			while(true){
-				String line = br.readLine();
-				// System.out.println(line);
+			// int x = 0;
 
-				if(line == null){
-					max_x = x;
-					break;
-				}
-				String[] lines = line.split(" ");
-				max_y = lines.length;
-				for(int y = 0; y < lines.length; y++){
-					// System.out.println(lines[y]);
-					if(!lines[y].equals("0")){
-						entrys.add(new Entry(x, y, Math.round(Float.parseFloat(lines[y])*100d)/100d));
-					}
-					
-				}
-				x++;
+			String first_line = br.readLine();
+			String first_element[] = first_line.split(" ");
+
+			max_x = Integer.parseInt(first_element[0]);
+			max_y = Integer.parseInt(first_element[1]);
+			count = Integer.parseInt(first_element[2]);
+
+			// entrys = new Entry[count];
+
+			for(int i = 0; i < count; i++){
+				String line = br.readLine();
+				String lines[] = line.split(" ");
+				int row = Integer.parseInt(lines[0]);
+				int col = Integer.parseInt(lines[1]);
+				double value = Math.round(Float.parseFloat(lines[2])*100d)/100d;
+				entrys.add(new Entry(row, col, value));
+
 			}
+			// while(true){
+			// 	String line = br.readLine();
+			// 	// System.out.println(line);
+
+			// 	if(line == null){
+			// 		// max_x = x;
+			// 		break;
+			// 	}
+			// 	String[] lines = line.split(" ");
+
+			// 	entrys.add(new Entry(Integer.parseInt(lines[0]), Integer.parseInt(lines[1]), Math.round(Float.parseFloat(lines[2])*100d)/100d));
+			// 	// max_y = lines.length;
+			// 	// for(int y = 0; y < lines.length; y++){
+			// 	// 	// System.out.println(lines[y]);
+			// 	// 	if(!lines[y].equals("0")){
+			// 	// 		entrys.add(new Entry(x, y, Math.round(Float.parseFloat(lines[y])*100d)/100d));
+			// 	// 	}
+					
+			// 	// }
+			// 	// x++;
+			// }
 		}
 		catch(Exception ex){
 			System.out.println(ex);
@@ -77,14 +142,75 @@ public class SparseMatrix  {
 			i.row = i.col;
 			i.col = temp;
 		}
+
+		// // row 정렬
+		// for(int i = 0; i < count - 1; i++){
+		// 	for(int j = 0; j < count - 1 - i; j++){
+		// 		if(entrys.get(j).row > entrys.get(j+1).row){
+		// 			Entry tmp = entrys.get(j);
+		// 			entrys.set(j, entrys.get(j+1));
+		// 			entrys.set(j+1, tmp);
+		// 		}
+		// 	}
+		// }
+
+
+		// int front = 0;
+		// int back = 0;
+
+		// // col 정렬
+
+		// for(int i = 0; i < count - 1; i++){
+		// 	if(entrys.get(i).row != entrys.get(i+1).row){
+		// 		back = i+1;
+		// 		entry_sort_part(front, back);
+		// 		front = back;
+		// 	}
+		// }
+		entry_sort();
+		print_arr();
 	}
 	
+	
+
+	public void print_arr(){
+		for(Entry e : entrys){
+			System.out.println(e.row+" "+e.col+" "+e.value);
+		}
+	}
 	/*
 	 * Add this matrix with another matrix m.
 	 * Assume that the dimensions of two matrices are always compatible.
 	 */
 	public void addTo(SparseMatrix m) {
-	
+		// int min_count = (m.count > count)? count : m.count;
+		// int max_count = (m.count > count)? m.count : count;
+
+		// for(int i = 0; i < count; i++){
+		// 	for(int j = 0; j < m.count; j++){
+		// 		if(entrys.get(i).row == m.entrys.get(j).row && entrys.get(i).col == m.entrys.get(j).col){
+		// 			entrys.get(i).value += m.entrys.get(j).value;
+		// 		}
+		// 	}
+		// }
+
+		for(int i = 0; i < m.count; i++){
+			boolean flag = false;
+			for(int j = 0; j < count; j++){
+				if(entrys.get(j).row == m.entrys.get(i).row && entrys.get(j).col == m.entrys.get(i).col){
+					entrys.get(j).value += m.entrys.get(i).value;
+					flag = true;
+					break;
+				}
+			}
+
+			if(!flag){
+				entrys.add(m.entrys.get(i));
+			}
+		}
+
+		entry_sort();
+		print_arr();
 	}
 	
 	/*
@@ -95,18 +221,31 @@ public class SparseMatrix  {
 		// 	i.printMe();	
 		// }
 		
+		// for(int i = 0; i < max_x; i++){
+		// 	for(int j = 0; j < max_y; j++){
+		// 		boolean flag = false;
+		// 		for(Entry e : entrys){
+		// 			if(e.row == i && e.col == j){
+		// 				flag = true;
+		// 				System.out.print(e.value+" ");
+		// 				break;
+		// 			}
+		// 		}
+
+		// 		if(!flag){
+		// 			System.out.print(0f+" ");
+		// 		}
+		// 	}
+		// 	System.out.println();
+		// }
+		int index = 0;
 		for(int i = 0; i < max_x; i++){
 			for(int j = 0; j < max_y; j++){
-				boolean flag = false;
-				for(Entry e : entrys){
-					if(e.row == i && e.col == j){
-						flag = true;
-						System.out.print(e.value+" ");
-						break;
-					}
+				if(entrys.get(index).row == i && entrys.get(index).col == j){
+					System.out.print(entrys.get(index).value+" ");
+					if(index < count-1) index++;
 				}
-
-				if(!flag){
+				else{
 					System.out.print(0f+" ");
 				}
 			}
