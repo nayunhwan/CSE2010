3124d4f91cb47c2dba830e3fee10de564b2a6800
NaYunHwan <kbk9288@gmail.com>
2015-09-22 16:05:41 +0900

    Commit
    Commit

--- HW/HW#1/SparseMatrix.java
+++ HW/HW#1/SparseMatrix.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class SparseMatrix  {
 	// nested class for each non-zero entry in Sparse Matrix
 
-	// ArrayList<Entry> entrys = new ArrayList<Entry>();
 	ArrayList<Entry> entrys = new ArrayList<Entry>();
 	int max_x, max_y, count;
 
@@ -33,14 +32,12 @@ public class SparseMatrix  {
 		entrys.set(index_j, tmp);
 	}
 
+	// Row 순으로 정렬하고, 구간을 잘라서 구간별로 Col을 기준으로 정렬
 	public void entry_sort(){
 		// row 정렬
 		for(int i = 0; i < entrys.size() - 1; i++){
 			for(int j = 0; j < entrys.size() - 1 - i; j++){
 				if(entrys.get(j).row > entrys.get(j+1).row){
-					// Entry tmp = entrys.get(j);
-					// entrys.set(j, entrys.get(j+1));
-					// entrys.set(j+1, tmp);
 					entry_swap(j,j+1);
 				}
 			}
@@ -51,26 +48,30 @@ public class SparseMatrix  {
 		int back = 0;
 
 		// col 정렬
-
-		for(int i = 0; i < entrys.size() - 1; i++){
+		for(int i = 0; i < entrys.size()-1; i++){
 			if(entrys.get(i).row != entrys.get(i+1).row){
+
 				back = i+1;
 				entry_sort_part(front, back);
 				front = back;
 			}
 		}
+		back = entrys.size();
+		entry_sort_part(front, back);
 	}
 
 	public void entry_sort_part(int front, int back){
-		for(int i = front; i < back - 1; i++){
-			for(int j = front; j < back - i - 1; j++){
+		// System.out.println("back2 "+(back-1));
+		// System.out.println("front : "+front+" back : "+back);
+		int count = 0;
+		for(int i = front; i < back-1; i++){
+			for(int j = front; j < back-1 - (i-front); j++){
 				if(entrys.get(j).col > entrys.get(j+1).col){
-					// Entry tmp = entrys.get(j);
-					// entrys.set(j, entrys.get(j+1));
-					// entrys.set(j+1, tmp);
+					// System.out.println("swap : "+j+" "+(j+1));
 					entry_swap(j,j+1);
 				}
 			}
+			// System.out.println();
 		}
 	}
 	
@@ -101,27 +102,6 @@ public class SparseMatrix  {
 				entrys.add(new Entry(row, col, value));
 
 			}
-			// while(true){
-			// 	String line = br.readLine();
-			// 	// System.out.println(line);
-
-			// 	if(line == null){
-			// 		// max_x = x;
-			// 		break;
-			// 	}
-			// 	String[] lines = line.split(" ");
-
-			// 	entrys.add(new Entry(Integer.parseInt(lines[0]), Integer.parseInt(lines[1]), Math.round(Float.parseFloat(lines[2])*100d)/100d));
-			// 	// max_y = lines.length;
-			// 	// for(int y = 0; y < lines.length; y++){
-			// 	// 	// System.out.println(lines[y]);
-			// 	// 	if(!lines[y].equals("0")){
-			// 	// 		entrys.add(new Entry(x, y, Math.round(Float.parseFloat(lines[y])*100d)/100d));
-			// 	// 	}
-					
-			// 	// }
-			// 	// x++;
-			// }
 		}
 		catch(Exception ex){
 			System.out.println(ex);
@@ -168,16 +148,9 @@ public class SparseMatrix  {
 		// 	}
 		// }
 		entry_sort();
-		print_arr();
 	}
 	
 	
-
-	public void print_arr(){
-		for(Entry e : entrys){
-			System.out.println(e.row+" "+e.col+" "+e.value);
-		}
-	}
 	/*
 	 * Add this matrix with another matrix m.
 	 * Assume that the dimensions of two matrices are always compatible.
@@ -197,29 +170,29 @@ public class SparseMatrix  {
 		for(int i = 0; i < m.count; i++){
 			boolean flag = false;
 			for(int j = 0; j < count; j++){
+				// A행렬의 원소와 B행렬의 원소 비교
 				if(entrys.get(j).row == m.entrys.get(i).row && entrys.get(j).col == m.entrys.get(i).col){
 					entrys.get(j).value += m.entrys.get(i).value;
 					flag = true;
 					break;
 				}
 			}
-
+			// A행렬에 B행렬 원소가 없을 경우 A행렬에 B행렬 원소 추가
 			if(!flag){
 				entrys.add(m.entrys.get(i));
 			}
 		}
 
 		entry_sort();
-		print_arr();
 	}
 	
 	/*
 	 * Print contents of this matrix
 	 */
 	public void printMe() {
-		// for(Entry i : entrys){
-		// 	i.printMe();	
-		// }
+		for(Entry i : entrys){
+			i.printMe();	
+		}
 		
 		// for(int i = 0; i < max_x; i++){
 		// 	for(int j = 0; j < max_y; j++){
@@ -238,18 +211,19 @@ public class SparseMatrix  {
 		// 	}
 		// 	System.out.println();
 		// }
-		int index = 0;
-		for(int i = 0; i < max_x; i++){
-			for(int j = 0; j < max_y; j++){
-				if(entrys.get(index).row == i && entrys.get(index).col == j){
-					System.out.print(entrys.get(index).value+" ");
-					if(index < count-1) index++;
-				}
-				else{
-					System.out.print(0f+" ");
-				}
-			}
-			System.out.println();
-		}
+		// int index = 0;
+		// for(int i = 0; i < max_x; i++){
+		// 	for(int j = 0; j < max_y; j++){
+		// 		if(entrys.get(index).row == i && entrys.get(index).col == j){
+		// 			System.out.print(entrys.get(index).value+" ");
+		// 			if(index < count-1) index++;
+		// 		}
+		// 		else{
+		// 			System.out.print(0f+" ");
+		// 		}
+		// 	}
+		// 	System.out.println();
+		// }
+
 	}
 }
